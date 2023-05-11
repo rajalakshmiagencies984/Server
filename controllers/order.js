a71d43a96c383b3342076dd401c20e97073bd89d
karthi0102 <karthirajendran12003@gmail.com>
2023-05-12 00:04:59 +0530

    order

--- controllers/order.js
+++ controllers/order.js
@@ -1,3 +1,4 @@
+const User=require('../model/User.js')
 const Order = require('../model/Orders')
 
 module.exports.newOrder=async(req,res)=>{
@@ -6,7 +7,32 @@ module.exports.newOrder=async(req,res)=>{
         await order.save();
         res.status(201).json(order);
     } catch (error) {
+        console.log(error)
         res.status(500).send(error)
     }
 }
 
+
+module.exports.getAll = async(req,res)=>{
+    try {
+        const order = await Order.find()
+        res.status(200).json(order)
+    } catch (error) {
+        console.log(error)
+        res.status(500).send(error)
+    }
+}
+
+
+module.exports.getMyOrders=async(req,res)=>{
+    const {id}=req.params;
+  
+    try {
+        const order=await Order.find().populate('user');
+        const orders = order.filter(o => o.user._id==id)
+
+        return res.status(200).json(orders)
+    } catch (error) {
+        res.status(500).send(error)
+    }
+}
--- routes/order.js
+++ routes/order.js
@@ -1,6 +1,9 @@
 const express = require("express")
 const router= express.Router();
+const {newOrder,getAll,getMyOrders}=require('../controllers/order')
 
-router.post('/')
+router.get('/',getAll)
+router.post('/',newOrder)
+router.get('/:id',getMyOrders)
 
 module.exports=router
